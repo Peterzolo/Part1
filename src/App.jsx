05d28213e7e30104dbdbcbd8ec45561961ec4a20
Peter <petersolomon704@gmail.com>
2023-03-20 23:10:51 +0100

    Added total number

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
   const exercises1 = 10;
-  // const part2 = "Using props to pass data";
-  // const exercises2 = 7;
-  // const part3 = "State of a component";
-  // const exercises3 = 14;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part1} exercises2={exercises1} />
-      <Content part3={part1} exercises3={exercises1} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
 };
