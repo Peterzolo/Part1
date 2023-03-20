b6267165c6d656416c5fb79c933544f8a5f04354
Peter <petersolomon704@gmail.com>
2023-03-20 23:05:12 +0100

    Modified content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Content from "./Content";
 import Header from "./Header";
 
 const App = () => {
   const course = "Half Stack application development";
-  // const part1 = "Fundamentals of React";
-  // const exercises1 = 10;
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
   // const part2 = "Using props to pass data";
   // const exercises2 = 7;
   // const part3 = "State of a component";
@@ -12,6 +13,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part1} exercises2={exercises1} />
+      <Content part3={part1} exercises3={exercises1} />
     </div>
   );
 };
