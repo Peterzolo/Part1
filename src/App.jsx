1d295a3fcf3a5d8024c930f4be3d16c0b35a415c
Peter <petersolomon704@gmail.com>
2023-03-22 15:57:58 +0100

    Update App.jsx
    Modified APP component

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,11 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <div>
+        <Header course={course} />
+        <Content parts={parts} />
+        <Total parts={parts} />
+      </div>
     </div>
   );
 };
